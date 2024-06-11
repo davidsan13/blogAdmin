957322acfeb965be8c6d2781c61b2c270422186c
David <sandavid08@yahoo.com>
2024-06-10 20:03:49 -0700

    created protected routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -8,53 +8,50 @@ import CreateBlog from "../pages/CreateBlog";
 import Blog from "../pages/Blog"
 import Login from '../pages/Login'
 import Logout from '../pages/Logout'
-import { createBrowserRouter, RouterProvider  } from "react-router-dom";
-import Cookies from 'js-cookie'
+import { createBrowserRouter, Route, RouterProvider  } from "react-router-dom";
+import Protected from "./RouteGuard";
 
 const Router =  () => {
-  const [token, setToken] = React.useState()
-  // setToken(Cookies.get('access_token'))
-
  
-  
-  React.useEffect(() => {
-    setToken(Cookies.get('access_token'))
-
-  },[])
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Layout/>,
+      element: <Protected/>,
       children: [
-        {
-          path: "/",
-          element: <Home />
-        },
-        {
-          path: "/login",
-          element: <Login/>
-        },
-        {
-          path:'/logout',
-          element: <Logout/>
-        },
-        {
-          path: "blog/create",
-          element: <CreateBlog/>
-        },
-        {
-          path:"blog/:blogId/edit",
-          element: <Edit/>
-        },
-        {
-          path: "blog/:blogId/delete",
-          element: <Delete/>
-        },
-        {
-          path: "blog/:blogId",
-          element: <Blog/>
+        { path: "/",
+          element: <Layout/>,
+          children: [
+          {
+            path: "/",
+            element: <Home/>
+          },
+         
+          {
+            path:'/logout',
+            element: <Logout/>
+          },
+          {
+            path: "blog/create",
+            element: <CreateBlog/>
+          },
+          {
+            path:"blog/:blogId/edit",
+            element: <Edit/>
+          },
+          {
+            path: "blog/:blogId/delete",
+            element: <Delete/>
+          },
+          {
+            path: "blog/:blogId",
+            element: <Blog/>
+          }
+        ]
         }
-      ]
+      ],
+    },
+    {
+      path:'/login',
+      element: <Login/>
     }
   ])
   return (
